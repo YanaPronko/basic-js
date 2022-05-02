dfe05cc32935ec100b337493b280629f0483e289
Yana Pronko <yaniar@tut.by>
2022-05-02 23:07:58 +0300

    feat: I done transform array!!!!

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,8 +14,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  const copy = arr.slice();
+  const result = [];
+  for (let i = 0; i < copy.length; i++) {
+    if (copy[i] == `--discard-next`) {
+      if (copy[i + 1] === undefined) {
+        continue;
+      } else {
+        result.push(copy[i + 1]);
+        result.pop();
+        i += 2;
+      }
+
+    } else if (copy[i] == `--discard-prev`) {
+      if (copy[i - 1] === undefined) {
+        continue;
+      } else {
+        result.splice((result.indexOf(copy[i - 1])), 1);
+      }
+    } else if (copy[i] == `--double-next`) {
+      if (copy[i + 1] === undefined) {
+        continue;
+      } else {
+        result.push(copy[i + 1]);
+      }
+    } else if (copy[i] == `--double-prev`) {
+      if (copy[i - 1] === undefined) {
+        continue;
+      } else {
+        result.push(copy[i - 1]);
+      }
+    } else {
+      result.push(copy[i]);
+    }
+    console.log(result);
+  }
+  return result;
  
 }
+
 
 module.exports = {
   transform
